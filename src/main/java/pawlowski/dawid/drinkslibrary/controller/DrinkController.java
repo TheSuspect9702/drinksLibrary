2a8dc22d5e1e7049114bd79edaed2fbed8366f00
Dawid Pawłowski <263937@student.pwr.edu.pl>
2023-11-08 14:36:04 +0100

    added Controller for Drinks

---
+++ src/main/java/pawlowski/dawid/drinkslibrary/controller/DrinkController.java
@@ -0,0 +1,41 @@
+package pawlowski.dawid.drinkslibrary.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pawlowski.dawid.drinkslibrary.model.Drink;
+import pawlowski.dawid.drinkslibrary.services.DrinkService;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/drink")
+public class DrinkController {
+
+    private final DrinkService drinkService;
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody Drink drink) {
+        Drink newDrink = drinkService.saveNewDrink(drink);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/drink/" + newDrink.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Drink> listDrinks() {return drinkService.listDrinks();}
+    @RequestMapping(value = "{drinkId}", method = RequestMethod.GET)
+    public Drink getDrinkById(@PathVariable("drinkId") UUID drinkId) {
+        log.debug("Get Drink by Id - in controller");
+
+        return drinkService.getDrinkById(drinkId);
+    }
+}
